8df97e773aef3e4f9f10446a1cdd4425ac9ff093
Nicola F. Salvaturi <muhammad.salvaturi@bankmas.co.id>
2026-01-27 21:27:26 +0700

    final adjust CI/CD with self-hosted runner

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 import 'di/injection.dart';
 
-  // -------------------------------
-  // TEST CACHED SELF HOSTED RUNNER
-  // -------------------------------
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
