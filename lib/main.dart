7ef211ed8c43da4532e47388de4804739ca64e8f
Nicola F. Salvaturi <muhammad.salvaturi@bankmas.co.id>
2026-05-22 10:56:22 +0700

    feat: implement crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:chatkuy/app_context.dart';
 import 'package:chatkuy/data/repositories/local_notification_repository.dart';
 import 'package:chatkuy/data/repositories/notification_repository.dart';
 import 'package:chatkuy/data/services/presence_service.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,27 +14,46 @@ import 'app.dart';
 import 'di/injection.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await setupDI();
-  await getIt<LocalNotificationRepository>().init();
-  await AppContext.init();
-  await getIt<NotificationRepository>().init();
-  getIt<PresenceService>().init();
+  var isCrashlyticsReady = false;
 
-  FirebaseMessaging.onMessage.listen((message) {
-    getIt<LocalNotificationRepository>().show(message);
-  });
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      isCrashlyticsReady = true;
 
-  FirebaseMessaging.onMessageOpenedApp.listen((message) {
-    getIt<NotificationRepository>().handleMessage(message);
-  });
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+      PlatformDispatcher.instance.onError = (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+        return true;
+      };
 
-  final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+      await setupDI();
+      await getIt<LocalNotificationRepository>().init();
+      await AppContext.init();
+      await getIt<NotificationRepository>().init();
+      getIt<PresenceService>().init();
 
-  if (initialMessage != null) {
-    getIt<NotificationRepository>().handleMessage(initialMessage);
-  }
+      FirebaseMessaging.onMessage.listen((message) {
+        getIt<LocalNotificationRepository>().show(message);
+      });
 
-  runApp(const MyApp());
+      FirebaseMessaging.onMessageOpenedApp.listen((message) {
+        getIt<NotificationRepository>().handleMessage(message);
+      });
+
+      final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+
+      if (initialMessage != null) {
+        getIt<NotificationRepository>().handleMessage(initialMessage);
+      }
+
+      runApp(const MyApp());
+    },
+    (error, stack) {
+      if (isCrashlyticsReady) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      }
+    },
+  );
 }
